059b67c42a17a8fe44c31b2fc8b37d4b130b0379
Tom Ross <tom.ross@protonmail.com>
2021-05-07 13:59:06 +0800

    Initial commit.

---
+++ src/hydrogenic_atom.f90
@@ -0,0 +1,294 @@
+!>
+program hydrogenic_atom
+  implicit none
+
+  use laguerre
+  use rsg
+
+  ! debugging flag
+  logical , parameter :: debugging = .true.
+
+  ! angular quantum number variables
+  integer :: l, m
+  real :: alpha
+
+  ! atomic variables
+  ! z - atomic charge
+  integer :: z
+
+  ! radial grid variables
+  integer :: n_r
+  real :: d_r, r_max
+  real , allocatable :: r_grid(:)
+
+  ! basis variables
+  integer :: n_basis
+  real , allocatable :: basis(:, :)
+  real , allocatable :: eigen_basis(:, :)
+
+  ! matrices
+  real , allocatable :: B(:, :), K(:, :), V(:, :), H(:, :)
+  real , allocatable :: eigen_values(:), eigen_vectors(:, :)
+
+  ! local variables
+  integer :: ii, jj, kk
+  integer :: ierr
+
+  ! read parameters from command line arguments
+  call read_input(l, m, alpha, z, n_basis, d_r, r_max)
+  n_r = ceiling(r_max / d_r)
+
+  ! allocate arrays
+  allocate(r_grid(n_r))
+
+  allocate(basis(n_r, n_basis))
+  allocate(eigen_basis(n_r, n_basis))
+
+  allocate(B(n_basis, n_basis))
+  allocate(K(n_basis, n_basis))
+  allocate(V(n_basis, n_basis))
+  allocate(H(n_basis, n_basis))
+
+  allocate(eigen_values(n_basis))
+  allocate(eigen_vectors(n_basis, n_basis))
+
+  ! initialise radial grid
+  do ii = 1, n_r
+    r_grid(ii) = d_r * ii
+  end do
+
+  ! calculate radial basis functions
+  call radial_basis(l, alpha, n_r, r_grid, n_basis, basis)
+
+  if (debugging) then
+    write (*, *) "basis(n_r, n_basis)"
+    call display_radial_basis(n_r, n_basis, basis)
+  end if
+
+  ! calculate matrix elements
+  call hydrogenic_matrices(l, m, alpha, z, n_basis, B, K, V, H)
+
+  if (debugging) then
+    write (*, *) "B(n_basis, n_basis)"
+    call display_matrix(n_basis, n_basis, B)
+
+    write (*, *) "K(n_basis, n_basis)"
+    call display_matrix(n_basis, n_basis, K)
+
+    write (*, *) "V(n_basis, n_basis)"
+    call display_matrix(n_basis, n_basis, V)
+
+    write (*, *) "H(n_basis, n_basis)"
+    call display_matrix(n_basis, n_basis, H)
+  end if
+
+  ! solve matrix equations
+  call rsg(n_basis, n_basis, H, B, eigen_values, 1, eigen_vectors, ierr)
+
+  if (ierr /= 0) then
+    write (*, "(a, i4)") "rsg failed with error code: ", ierr
+    call exit(ierr)
+  end if
+
+  if (debugging) then
+    write (*, *) "eigen_values(n_basis)"
+    call display_vector(n_basis, eigen_values)
+  end if
+
+  ! calculate eigen-states
+  eigen_basis(:, :) = 0.0
+
+  do jj = 1, n_basis
+    do ii = 1, n_r
+      do kk = 1, n_basis
+        eigen_basis(ii, jj) = eigen_basis(ii, jj) &
+            + (eigen_vectors(kk, jj) * basis(ii, kk))
+      end do
+    end do
+  end do
+
+  if (debugging) then
+    write (*, *) "eigen_basis(n_r, n_basis)"
+    call display_radial_basis(n_r, n_basis, eigen_basis)
+  end if
+
+  ! write output to file
+
+  ! deallocate arrays
+  deallocate(r_grid)
+
+  deallocate(basis)
+  deallocate(eigen_basis)
+
+  deallocate(B)
+  deallocate(K)
+  deallocate(V)
+  deallocate(H)
+
+  deallocate(eigen_values)
+  deallocate(eigen_vectors)
+
+contains
+
+  ! read_input
+  subroutine read_input (l, m, alpha, z, n_basis, d_r, r_max)
+    integer , intent(out) :: l, m, z, n_basis
+    real , intent(out) :: alpha, d_r, r_max
+    integer :: num_args
+    character(len=20) :: arg
+
+    num_args = command_argument_count()
+
+    if (num_args >= 1) then
+      call get_command_argument(1, arg)
+      read (arg, *) l
+    else
+      write (*, *) "<l> not specified, using default value of 0"
+      l = 0
+    end if
+
+    if (num_args >= 2) then
+      call get_command_argument(2, arg)
+      read (arg, *) m
+    else
+      write (*, *) "<m> not specified, using default value of 0"
+      m = 0
+    end if
+
+    if (num_args >= 3) then
+      call get_command_argument(3, arg)
+      read (arg, *) alpha
+    else
+      write (*, *) "<alpha> not specified, using default value of 1.0"
+      alpha = 1.0
+    end if
+
+    if (num_args >= 4) then
+      call get_command_argument(4, arg)
+      read (arg, *) z
+    else
+      write (*, *) "<z> not specified, using default value of 1"
+      z = 1
+    end if
+
+    if (num_args >= 5) then
+      call get_command_argument(5, arg)
+      read (arg, *) n_basis
+    else
+      write (*, *) "<n_basis> not specified, using default value of 10"
+      n_basis = 10
+    end if
+
+    if (num_args >= 6) then
+      call get_command_argument(6, arg)
+      read (arg, *) d_r
+    else
+      write (*, *) "<d_r> not specified, using default value of 0.01"
+      d_r = 0.01
+    end if
+
+    if (num_args >= 7) then
+      call get_command_argument(7, arg)
+      read (arg, *) r_max
+    else
+      write (*, *) "<r_max> not specified, using default value of 100.0"
+      r_max = 100.0
+    end if
+
+  end subroutine read_input
+
+  ! write_output
+  ! subroutine write_output ()
+  ! end subroutine write_output
+
+  ! display_vector
+  subroutine display_vector (n, x)
+    integer , intent(in) :: n
+    real , intent(in) :: x(n)
+    integer :: ii
+    integer :: w, d
+    character(len=50) :: fmt, str_w, str_d
+
+    ! determine real number formatting
+    d = 4
+    w = ceiling(log10(max(abs(x(:))))) + d
+
+    write (str_w, *) w
+    write (str_d, *) d
+
+    write (fmt, *) "(f", trim(adustl(str_w)), ".", trim(adustl(str_d)), ")"
+
+    write (*, *) fmt
+
+    ! write out matrix elements
+    do ii = 1, n_rows
+      ! todo: if x(ii) will be written as "0.00..0", replace with "     ."
+      write (*, fmt) x(ii)
+    end do
+
+  end subroutine display_vector
+
+  ! display_matrix
+  subroutine display_matrix (n_rows, n_cols, A)
+    integer , intent(in) :: n_rows, n_cols
+    real , intent(in) :: A(n_rows, n_cols)
+    integer :: ii, jj
+    integer :: w, d
+    character(len=50) :: fmt, str_w, str_d
+
+    ! determine real number formatting
+    d = 4
+    w = ceiling(log10(max(abs(A(:, :))))) + d
+
+    write (str_w, *) w
+    write (str_d, *) d
+
+    write (fmt, *) "(f", trim(adustl(str_w)), ".", trim(adustl(str_d)), ")"
+
+    write (*, *) fmt
+
+    ! write out matrix elements
+    do ii = 1, n_rows
+      do jj = 1, n_cols
+        ! todo: if A(ii, jj) will be written as "0.00..0", replace with "     ."
+        write (*, fmt, advance="no") A(ii, jj)
+        write (*, *, advance="no") " "
+      end do
+      write (*, *)
+    end do
+
+  end subroutine display_matrix
+
+  ! display_radial_basis
+  subroutine display_radial_basis (n_r, n_basis, basis)
+    integer , intent(in) :: n_r, n_basis
+    real , intent(in) :: basis(n_r, n_basis)
+    integer :: ii, jj
+    integer :: w, d
+    character(len=50) :: fmt, str_w, str_d
+
+    ! determine real number formatting
+    d = 4
+    w = ceiling(log10(max(abs(basis(:, :))))) + d
+
+    write (str_w, *) w
+    write (str_d, *) d
+
+    write (fmt, *) "(f", trim(adustl(str_w)), ".", trim(adustl(str_d)), ")"
+
+    write (*, *) fmt
+
+    ! write out radial basis values
+    do ii = 1, n_r
+      do jj = 1, n_basis
+        ! todo: if basis(ii, jj) will be written as "0.00..0", replace with
+        ! "     ."
+        write (*, fmt, advance="no") basis(ii, jj)
+        write (*, *, advance="no")  " "
+      end do
+      write (*, *)
+    end do
+
+  end subroutine display_radial_basis
+
+end program hydrogenic_atom
---
+++ src/laguerre.f90
@@ -0,0 +1,166 @@
+!>
+module laguerre
+  implicit none
+
+contains
+
+  ! radial_basis
+  !
+  ! phi_{k, l, m}(r, theta, phi) = (varphi_{k, l}(r) / r) * Y_{l, m}(theta, phi)
+  ! where
+  ! varphi_{k, l}(r) = sqrt(alpha * (k - 1)! / (k + l) * (k + 2*l)!)
+  !                    * (2*alpha*r)^{l+1}
+  !                    * exp(-alpha*r)
+  !                    * L_{k - 1}^{2*l + 1}(2*alpha*r)
+  ! where L_{i}^{j} are the generalised Laguerre polynomials.
+  !
+  ! For given l, alpha, and r_grid, yields the functions varphi_{k, l}(r) for
+  ! k = 1, ..., n_basis, on the radial values specified in the grid.
+  pure subroutine radial_basis (l, alpha, n_r, r_grid, n_basis, basis)
+    integer , intent(in) :: l, n_r, n_basis
+    real , intent(in) :: alpha
+    real , intent(in) :: r_grid(n_r)
+    real , intent(out) :: basis(n_r, n_basis)
+    real :: norm(n_basis)
+    real :: alpha_grid(n_r)
+    integer :: kk
+
+    ! in-lined array since r_grid(:) on its own is never used
+    alpha_grid(:) = alpha * r_grid(:)
+
+    ! recurrence relation for basis functions
+    basis(:, 1) = ((2.0 * alpha_grid(:)) ** (l + 1)) * &
+        exp(-alpha_grid(:))
+
+    basis(:, 2) = 2.0 * (l + 1 - alpha_grid(:)) * basis(:, 1)
+
+    do kk = 3, n_basis
+      basis(:, kk) = ((2.0 * (kk - 1 + l - alpha_grid(:)) * basis(:, kk-1)) &
+          - (kk + (2 * l) - 1) * basis(:, kk-2)) / real(kk - 1)
+    end do
+
+    ! recurrence relation for basis normalisation constants
+    norm(1) = sqrt(alpha / real((l + 1) * gamma((2 * l) + 2)))
+
+    do kk = 2, n_basis
+      norm(kk) = norm(kk-1) * sqrt(real(kk * (kk + l)) / &
+          real((kk + l + 1) * (kk + (2 * l) + 1)))
+    end do
+
+    ! scaling basis functions by normalisation constants
+    do kk = 1, n_basis
+      basis(:, kk) = basis(:, kk) * norm(kk)
+    end do
+
+  end subroutine radial_basis
+
+  ! overlap_matrix
+  !
+  ! < phi_{k', l, m} | phi_{k, l, m} >
+  !
+  ! Overlap matrix elements for given l, m.
+  ! We can restrict our attention to considering fixed l and m, since the matrix
+  ! elements are zero when l' /= l or where m' /= m.
+  ! Furthermore, the exponential decay variable, alpha, has no influence on
+  ! these matrix elements.
+  subroutine overlap_matrix(l, m, n_basis, B)
+    integer , intent(in) :: l, m, n_basis
+    real , intent(out) :: B(n_basis, n_basis)
+    integer :: kk
+
+    ! initialise overlap matrix to zero
+    B(:, :) = 0.0
+
+    ! determine tri-diagonal overlap matrix elements
+    do kk = 1, n_basis-1
+      B(kk, kk) = 1.0
+
+      B(kk, kk+1) = - 0.5 * sqrt(1 - &
+          (real(l * (l + 1)) / real((kk + l) * (kk + l + 1))))
+
+      B(kk+1, kk) = B(kk, kk+1)
+    end do
+
+    ! last term (not covered by loop)
+    B(n_basis, n_basis) = 1.0
+
+  end subroutine overlap_matrix
+
+  ! kinetic_matrix
+  !
+  ! < phi_{k', l, m} | K | phi_{k, l, m} >
+  !
+  ! Kinetic matrix elements for given l, m, alpha.
+  ! We can restrict our attention to considering fixed l and m, since the matrix
+  ! elements are zero when l' /= l or where m' /= m.
+  subroutine kinetic_matrix(l, m, alpha, n_basis, K)
+    integer , intent(in) :: l, m, n_basis
+    real , intent(in) :: alpha
+    real , intent(out) :: K(n_basis, n_basis)
+    integer :: kk
+
+    ! initialise kinetic matrix to zero
+    K(:, :) = 0.0
+
+    ! determine tri-diagonal kinetic matrix elements
+    do kk = 1, n_basis-1
+      K(kk, kk) = 0.5 * (alpha ** 2)
+
+      K(kk, kk+1) = (alpha ** 2) * 0.25 * sqrt(1 - &
+          (real(l * (l + 1)) / real((kk + l) * (kk + l + 1))))
+
+      K(kk+1, kk) = K(kk, kk+1)
+    end do
+
+    ! last term (not covered by loop)
+    K(n_basis, n_basis) = 0.5 * (alpha ** 2)
+
+  end subroutine kinetic_matrix
+
+  ! coulomb_matrix
+  !
+  ! < phi_{k', l, m} | 1/r | phi_{k, l, m} >
+  !
+  ! Coulomb matrix elements for given l, m, alpha.
+  ! We can restrict our attention to considering fixed l and m, since the matrix
+  ! elements are zero when l' /= l or where m' /= m.
+  subroutine coulomb_matrix(l, m, alpha, n_basis, V)
+    integer , intent(in) :: l, m, n_basis
+    real , intent(in) :: alpha
+    real , intent(out) :: V(n_basis, n_basis)
+    integer :: kk
+
+    ! initialise coulomb matrix to zero
+    V(:, :) = 0.0
+
+    ! determine diagonal coulomb matrix elements
+    do kk = 1, n_basis
+      V(kk, kk) = alpha / (kk + l)
+    end do
+
+  end subroutine coulomb_matrix
+
+  ! hydrogenic_matrices
+  !
+  ! Yields overlap, kinetic, potential and Hamiltonian matrices for given l, m,
+  ! alpha, z (atomic charge); that is: B, K, V, H.
+  ! We can restrict our attention to considering fixed l and m, since the matrix
+  ! elements are zero when l' /= l or where m' /= m.
+  subroutine hydrogenic_matrices(l, m, alpha, z, n_basis, B, K, V, H)
+    integer , intent(in) :: l, m, z, n_basis
+    real , intent(in) :: alpha
+    real , intent(out) :: B(n_basis, n_basis), K(n_basis, n_basis), &
+        V(n_basis, n_basis), H(n_basis, n_basis)
+
+    call overlap_matrix(l, m, n_basis, B)
+
+    call kinetic_matrix(l, m, alpha, n_basis, K)
+
+    call coulomb_matrix(l, m, alpha, n_basis, V)
+    V(:, :) = - real(z) * V(:, :)
+
+    H(:, :) = K(:, :) + V(:, :)
+
+  end subroutine hydrogenic_matrices
+
+end module laguerre
