180aac056032dfcad2b449e774ef85d6ea865584
Tom Ross <tom.ross@protonmail.com>
2021-05-08 16:26:09 +0800

    Created a job script to execute bin/hydrogenic_atom for various sets of parameters.

---
+++ jobs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# constant parameters
+m=0
+atomic_charge=1
+d_r=0.1
+r_max=100.0
+
+# parameter sets
+n_basis_set="1 2 4 8 16 32 64"
+l_set="0 1 2 3"
+alpha_set="0.75 1.0 1.25"
+
+# compile
+make hydrogenic_atom
+
+# run jobs
+for n_basis in ${n_basis_set} ; do
+    for l in ${l_set} ; do
+        for alpha in ${alpha_set} ; do
+            bin/hydrogenic_atom ${l} ${m} ${alpha} ${atomic_charge} \
+                                ${n_basis} ${d_r} ${r_max}
+
+        done
+    done
+done
