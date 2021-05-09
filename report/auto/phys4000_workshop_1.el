05fdc7bce0a5de8b89e91a2fd1506938faf61a29
Tom Ross <tom.ross@protonmail.com>
2021-05-09 14:31:48 +0800

    Initialising report directory.

---
+++ report/auto/phys4000_workshop_1.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "phys4000_workshop_1"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "draft")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "base")
+   (LaTeX-add-labels
+    "sec:introduction"
+    "sec:laguerre-basis"
+    "sec:analyt-matr-elem"
+    "sec:extens-overl-matr"
+    "sec:atom-hydr-stat"
+    "sec:he+-ion"
+    "sec:surface-plot-xz"
+    "sec:numer-calc-potent"))
+ :latex)
+
